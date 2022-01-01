62ec2798fe219649df1ca3c709e2754bdc913647
Trym Nilsen <trym.nilsen@gmail.com>
2022-01-01 20:11:18 +0100

    add estlint config

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+    root: true,
+    env: {
+        es6: true,
+        node: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:import/typescript",
+        "google",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: ["tsconfig.json", "tsconfig.dev.json"],
+        sourceType: "module",
+    },
+    ignorePatterns: [
+        "/lib/**/*", // Ignore built files.
+    ],
+    plugins: ["@typescript-eslint", "import"],
+    rules: {
+        indent: ["error", 4, { SwitchCase: 1 }],
+        "no-invalid-this": 0,
+        "object-curly-spacing": 0,
+        "import/no-unresolved": 0,
+        "require-jsdoc": 0,
+        "new-cap": 0,
+        "spaced-comment": 0,
+        "max-len": [
+            "warn",
+            {
+                code: 90,
+                ignoreTemplateLiterals: true,
+                ignoreStrings: true,
+                ignoreUrls: true,
+            },
+        ],
+        quotes: ["error", "double", { allowTemplateLiterals: true }],
+        "prefer-promise-reject-errors": 0,
+        "valid-jsdoc": [
+            "error",
+            { requireParamType: false, requireReturnType: false },
+        ],
+        camelcase: 0,
+    },
+};
