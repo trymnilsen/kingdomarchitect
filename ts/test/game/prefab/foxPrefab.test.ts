e17f492bfce78ccfaa84caf56a55e9f754e2d4fa
Trym Nilsen <trym.nilsen@gmail.com>
2025-10-02 21:20:34 +0200

    removed old tests

--- ts/test/game/prefab/foxPrefab.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it } from "node:test";
-import assert from "node:assert";
-
-describe("Fox prefab", () => {
-    it("Can persist and load", () => {
-        assert.strictEqual(2, 2);
-    });
-});
